4761eafe617a7da45314663edcc862e57f20f9fe
Gaurav Singh <gaurav.singh@go-jek.com>
2019-09-27 11:00:25 +0530

    Chapter 4 + 5: Initializing eyes in your tests and running your first hello world test.

--- automation/tests/chapter_04/hello_world.py
+++ automation/tests/chapter_04/hello_world.py
@@ -4,4 +4,10 @@ from automation.tests.base_test import BaseTest
 class HelloWorldTest(BaseTest):
 
     def test_hello_world(self):
-        pass
+        self.setup(test_name=self._testMethodName)
+
+        self.validate_window(tag='hello_world')
+        self.driver.find_element_by_css_selector('button').click()
+        self.validate_window('click_me')
+
+        self.teardown()
